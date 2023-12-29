511773b102ab02c4c629995a22173671edcbb522
nnan0624 <153707817+nnan0624@users.noreply.github.com>
2023-12-29 12:05:40 +0700

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -108,7 +108,21 @@ ORDER BY avg(rating) DESC, m.title
 LIMIT 1)
 
 --ex12
-
+WITH FriendCounts AS (SELECT
+    id,
+    COUNT(DISTINCT friend_id) AS num
+FROM
+    ((SELECT requester_id AS id, accepter_id AS friend_id FROM RequestAccepted)
+       UNION ALL 
+      (SELECT accepter_id AS id, requester_id AS friend_id FROM RequestAccepted)) AS friendships
+GROUP BY id
+)
+SELECT
+  id,
+  num
+FROM FriendCounts
+ORDER BY num DESC
+LIMIT 1
 
 
 
